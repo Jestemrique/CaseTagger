8b61e0752501c9f034c912479724cf8897d28ae8
Enrique <enrique.pastrana@gmail.com>
2019-04-10 18:13:18 +0200

    Removing unnecessary files

--- options.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("settingsButton").addEventListener("click", (e) =>{
-    e.preventDefault();
-    let userInput = document.querySelector("#txtHost").nodeValue;
-    console.log(userInput);
-});
-
-
-
