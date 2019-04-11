31185756da1c7ca10491dfad01fb382f4b07bec2
Enrique <enrique.pastrana@gmail.com>
2019-04-11 15:48:22 +0200

    Working in content script to retriev currentuser

--- contentScript.js
+++ contentScript.js
@@ -11,4 +11,12 @@ alert("Hola desde content!");
 var els = document.querySelectorAll("a[href^='http://devappcentral:8383/CSRallyGate/user/mpastrana/searcher/true']");
 debugger;
 console.log("enlace: " + els);
+//var currentUser=window.Aura.initConfig.context.globalValueProviders[0].values.CurrentUser.Email;
+//console.log("Current user: " + window.Aura.initConfig.context.globalValueProviders[0].values.CurrentUser.Email);
+const divs = document.getElementsByTagName("a");
+console.log("enlaces: " + divs);
+
+
+
+
 
