d36ca136fe44afd9096ec1276eecb38b4837c03b
Enrique <enrique.pastrana@gmail.com>
2019-04-18 16:51:11 +0200

    Retrieving current user

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,34 @@
+// create an observer instance
+
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        console.log(mutation);
+        /*
+        var p = document.createElement("p");
+        p.innerHTML = "type: " + mutation.type + "<br />" +
+            "target id: " + mutation.target.id + "<br />" +
+            "target attribute foo: " + mutation.target.attributes['foo'].value + "<br />";
+            
+        document.querySelector('.console').appendChild(p);
+        */
+    });
+});
+//target
+var target = document.body;
+// configuration of the observer:
+var config = {
+    attributes: true,
+    childList: true,
+    characterData: true
+};
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+
+// later, you can stop observing
+//observer.disconnect();
+
+
 /*
 document.addEventListener('readystatechange', event => {
     if (event.target.readyState === 'interactive') {
@@ -28,7 +59,7 @@ console.log("Start logging!");
 
 
 
-
+/*
 function DOMtoString(document_root) {
     var html = '',
         node = document_root.firstChild;
@@ -48,6 +79,7 @@ function DOMtoString(document_root) {
             break;
         case Node.DOCUMENT_TYPE_NODE:
             // (X)HTML documents are identified by public identifiers
+            debugger;
             html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
             break;
         }
@@ -55,11 +87,11 @@ function DOMtoString(document_root) {
     }
     return html;
 }
-
+debugger;
 const source = DOMtoString(document);
 console.log("HERE'S THE MATCH");
 console.log(source.match(/"@microstrategy.com"/));
+console.log(document);
 
-
-
+*/
 
